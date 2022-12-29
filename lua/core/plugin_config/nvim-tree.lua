3b3e29f7606abf45a95560514f1c3ecd9bc429c6
Jphn <joaopedroholandaneves@gmail.com>
2022-12-29 16:21:19 -0300

    nvim-tree: filter dotfiles

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,27 +1,30 @@
 require("nvim-tree").setup({
-	view = {
-		adaptive_size = true,
-		float = {
-			enable = true,
-		},
-	},
-	renderer = {
-		highlight_opened_files = "all",
-		indent_markers = {
-			enable = true,
-			icons = {
-				corner = "└ ",
-				edge = "│ ",
-				item = "│ ",
-				none = "  ",
-			},
-		},
-	},
-	actions = {
-		open_file = {
-			quit_on_open = true,
-		},
-	},
+  view = {
+    adaptive_size = true,
+    float = {
+      enable = true,
+    },
+  },
+  renderer = {
+    highlight_opened_files = "all",
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        item = "│ ",
+        none = "  ",
+      },
+    },
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+  filters = {
+    dotfiles = true,
+  },
 })
 
 vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeFindFileToggle<CR>")
