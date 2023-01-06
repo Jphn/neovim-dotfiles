fba86af828a38e4a5837866c196372389efb0921
Jphn <joaopedroholandaneves@gmail.com>
2023-01-06 15:09:42 -0300

    plugin(mason): ensure installed languages

--- lua/core/plugin_config/lsp/default.lua
+++ lua/core/plugin_config/lsp/default.lua
@@ -1,3 +1,4 @@
 require("mason-lspconfig").setup({
-	ensure_installed = { "sumneko_lua", "tsserver", "html", "cssls" },
+  ensure_installed = { "sumneko_lua", "tsserver", "html", "cssls" },
+  automatic_installation = true,
 })
