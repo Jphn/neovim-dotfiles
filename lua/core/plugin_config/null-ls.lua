e193b0ccccca2b56f0a1f70cbd02e81abea3250a
Jphn <joaopedroholandaneves@gmail.com>
2023-01-14 12:00:10 -0300

    plugin(null-ls): add autopep8

--- lua/core/plugin_config/null-ls.lua
+++ lua/core/plugin_config/null-ls.lua
@@ -23,6 +23,7 @@ null_ls.setup({
 		----------------
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.autopep8,
 
 		-----------------
 		-- Diagnostics --
