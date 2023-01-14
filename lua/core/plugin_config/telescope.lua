9306eb35f220877568f82c97fedfd2528318c948
Jphn <joaopedroholandaneves@gmail.com>
2023-01-14 12:01:06 -0300

    plugin(telescope): ignore node_modules

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,3 +1,9 @@
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "node_modules" },
+	},
+})
+
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
