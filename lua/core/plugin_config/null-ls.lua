0f38601d4bef24150f8cfc6186f2155d7ce7b519
Jphn <joaopedroholandaneves@gmail.com>
2023-01-16 23:08:24 -0300

    plguin(null-ls): add deno_fmt source

--- lua/core/plugin_config/null-ls.lua
+++ lua/core/plugin_config/null-ls.lua
@@ -24,6 +24,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.formatting.deno_fmt,
 
 		-----------------
 		-- Diagnostics --
