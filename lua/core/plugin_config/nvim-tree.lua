2ce69d9e829ecbb9de7e975a4692e90e44bb93ee
Jphn <joaopedroholandaneves@gmail.com>
2023-01-16 23:11:59 -0300

    indentation: update nvim-tree file to new indentation

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,4 +1,30 @@
 require("nvim-tree").setup({
+	view = {
+		adaptive_size = true,
+		float = {
+			enable = true,
+		},
+	},
+	renderer = {
+		highlight_opened_files = "all",
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				item = "│ ",
+				none = "  ",
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
 
 local map = require("core.utils").map
