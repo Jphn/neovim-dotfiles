294a42d1d6a119b331d45720f7bada82b63af307
Jphn <joaopedroholandaneves@gmail.com>
2023-01-16 23:15:06 -0300

    indentation(tests): update ts file to new identation

--- test/index.ts
+++ test/index.ts
@@ -1,25 +1,24 @@
 function sum(n1: number, n2: number): number {
-  return n1 + n2;
+	return n1 + n2;
 }
 
-sum(1, 2)
-sum(1, '2')
+sum(1, 2);
 
 // Try creating a simple object
 const foo = {
-  bar: 'John Doe'
-}
+	bar: 'John Doe',
+};
 
 class Person {
-  constructor(public name: string, private _age: number) {}
+	constructor(public name: string, private _age: number) { }
 
-  public set age(age: number) {
-    if (age > 0) this._age = age;
-  }
+	public set age(age: number) {
+		if (age > 0) this._age = age;
+	}
 
-  public get age() {
-    return this._age;
-  }
+	public get age() {
+		return this._age;
+	}
 }
 
 const person1 = new Person();
