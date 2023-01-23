0d1e0e67150d8feb25e4bde4bca204b0e43c5e93
Jphn <joaopedroholandaneves@gmail.com>
2023-01-23 18:11:49 -0300

    fix(lsp): compatibility between tsserver and denols #3

--- lua/core/plugin_config/lsp/servers.lua
+++ lua/core/plugin_config/lsp/servers.lua
@@ -4,7 +4,7 @@ local lsp = require("lspconfig")
 local util = lsp.util
 
 local map = require("core.utils").map
-local on_attach = function(_, _)
+local on_attach = function(client, _)
 	map("n", "<leader>rn", vim.lsp.buf.rename)
 	map("n", "<leader>ca", vim.lsp.buf.code_action)
 
@@ -13,6 +13,23 @@ local on_attach = function(_, _)
 	map("n", "gr", require("telescope.builtin").lsp_references)
 	map("n", "K", vim.lsp.buf.hover)
 	-- map("i", "<C-h>", vim.lsp.buf.signature_help)
+
+	local active_clients = vim.lsp.get_active_clients()
+	if client.name == "denols" then
+		for _, client_ in pairs(active_clients) do
+			-- stop tsserver if denols is already active
+			if client_.name == "tsserver" then
+				client_.stop()
+			end
+		end
+	elseif client.name == "tsserver" then
+		for _, client_ in pairs(active_clients) do
+			-- prevent tsserver from starting if denols is already active
+			if client_.name == "denols" then
+				client.stop()
+			end
+		end
+	end
 end
 
 -----------------------
