90a17cb3c41aaec359ddfb912b2d03dd811f4c31
Jphn <joaopedroholandaneves@gmail.com>
2023-01-25 21:07:04 -0300

    fix(nvim-tree): remap LEADER-e to NvimTreeFindFileToggle

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -30,5 +30,5 @@ require("nvim-tree").setup({
 
 local map = require("core.utils").map
 
-map("n", "<leader>e", "<CMD>NvimTreeToggle<CR>")
+map("n", "<leader>e", "<CMD>NvimTreeFindFileToggle<CR>")
 -- map("n", "<leader>o", "<CMD>NvimTreeFocus<CR>")
